55dd49b312bf4c44c4468074dedb63cdbcf78037
Armagan Dalkiran <armagandalkran20@gmail.com>
2022-06-11 11:36:51 +0300

    overflow bug fixed

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ this.onload = (event) => {
   document.querySelector("body").classList.remove("preload");
 };
 const sliders = document.querySelectorAll(".slide-in");
-const appearOptions = { threshold: 0.1, rootMargin: "0px 0px -100px 0px" };
+const appearOptions = { threshold: 0, rootMargin: "0px 0px -250px 0px" };
 const appearOnScroll = new IntersectionObserver((entries, appearOnScroll) => {
   for (entry of entries) {
     if (!entry.isIntersecting) {
